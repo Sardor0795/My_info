3b3d620cc9b4737dd4c159047d27be7400cba1d1
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-01-03 08:38:22 +0500

    js

---
+++ js_info/1.2.js
@@ -0,0 +1,3 @@
+// MDN (Mozilla) JavaScript Reference is the main manual with examples and other information. It’s great to get in-depth information about individual language functions, methods etc.
+
+// You can find it at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference.
