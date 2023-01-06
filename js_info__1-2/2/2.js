3229aa9285b680108a00cf5861789f7433d00cbe
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-01-06 15:51:19 +0500

    info

---
+++ js_info__1-2/2/2.js
@@ -0,0 +1,17 @@
+/*
+
+STATEMENT
+
+Semicolon must have  -;- when statement end
+if not error
+enter understood as semicolon -- automatic semicolon insertion
+
+[] dont put ; in line break
+
+asterisk
+
+nested comments accurs error
+
+comments do not have negative effects on production at all
+
+*/
