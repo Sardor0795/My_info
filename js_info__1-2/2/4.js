a3a763b57a322c73508c25de26214148104c362b
Sardor0795 <english.programmer.9595@gmail.com>
2023-01-07 22:51:35 +0500

    info

---
+++ js_info__1-2/2/4.js
@@ -0,0 +1,18 @@
+// let The statement below creates (in other words: declares) a variable with the name “message”:
+
+// What’s interesting – the dollar sign '$' and the underscore '_' can also be used in names. They are regular symbols, just like letters, without any special meaning.
+
+// let 1a; // cannot start with a digit
+
+// let my-name; // hyphens '-' aren't allowed in the name
+
+// Case matters
+// Variables named apple and APPLE are two different variables.
+
+// Non-Latin letters are allowed, but not recommended
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#Keywords
+
+// An assignment without use strict
+
+// const uppercase in hard-coded
