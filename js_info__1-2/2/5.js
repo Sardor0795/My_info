329daf50f4f98c066509e10d1ad4ba045f10ad47
Sardor0795 <english.programmer.9595@gmail.com>
2023-01-08 09:16:05 +0500

    info

---
+++ js_info__1-2/2/5.js
@@ -0,0 +1,12 @@
+// There are 8 basic data types in JavaScript
+
+// Besides regular numbers, there are so-called “special numeric values” which also belong to this data type: Infinity, -Infinity and NaN.
+
+/*
+console.log(Infinity < 20);
+
+So, if there’s a NaN somewhere in a mathematical expression, it propagates to the whole result (there’s only one exception to that: NaN ** 0 is 1).
+
+
+In JavaScript, the “number” type cannot safely represent integer values larger than (253-1) (that’s 9007199254740991), or less than -(253-1) for negatives.
+*/
