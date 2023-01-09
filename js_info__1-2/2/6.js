7370a9c301529972c9a5e39be2c8421df01c553e
Sardor0795 <english.programmer.9595@gmail.com>
2023-01-09 13:45:31 +0500

    info

---
+++ js_info__1-2/2/6.js
@@ -0,0 +1,11 @@
+/*
+
+The mini-window with the message is called a modal window. The word “modal” means that the visitor can’t interact with the rest of the page, press other buttons, etc, until they have dealt with the window. In this case – until they press “OK”.
+
+The call to prompt returns the text from the input field or null if the input was canceled.
+
+shows a message and waits for the user to press “OK” or “Cancel”. It returns true for OK and false for Cancel/Esc.
+
+
+
+*/
