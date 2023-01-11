7d6d3cd70dfc387d6c843713b31f711f6c34815c
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-01-11 15:38:25 +0500

    info

---
+++ js_info__1-2/2/7.js
@@ -0,0 +1,15 @@
+/*
+
+Value	Becomes…
+undefined	NaN
+null	0
+true and false	1 and 0
+string	Whitespaces (includes spaces, tabs \t, newlines \n etc.) from the start and end are removed. If the remaining string is empty, the result is 0. Otherwise, the number is “read” from the string. An error gives NaN.
+
+
+
+Value	Becomes…
+0, null, undefined, NaN, ""  ->	false
+any other value	-> true
+
+*/
