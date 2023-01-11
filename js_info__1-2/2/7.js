a3aa81cc49d46d039456b8b2bf8ce24dc1052deb
Sardor0795 <english.programmer.9595@gmail.com>
2023-01-11 17:05:21 +0500

    info

--- js_info__1-2/2/7.js
+++ js_info__1-2/2/7.js
@@ -1,26 +1,14 @@
 /*
 
-<<<<<<< HEAD
-
 Value	Becomes…
 undefined	NaN
 null	0
-' '     0
-=======
-Value	Becomes…
-undefined	NaN
-null	0
->>>>>>> a3b4d331bc95bcc3f9289a13e2241dabd2a5c00d
 true and false	1 and 0
 string	Whitespaces (includes spaces, tabs \t, newlines \n etc.) from the start and end are removed. If the remaining string is empty, the result is 0. Otherwise, the number is “read” from the string. An error gives NaN.
 
-
-<<<<<<< HEAD
-=======
 
 Value	Becomes…
 0, null, undefined, NaN, ""  ->	false
 any other value	-> true
 
->>>>>>> a3b4d331bc95bcc3f9289a13e2241dabd2a5c00d
 */
