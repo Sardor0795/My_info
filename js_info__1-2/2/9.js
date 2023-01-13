ae00719a32011c94029eb6912e42830e5f753bc2
Sardor0795 <english.programmer.9595@gmail.com>
2023-01-13 17:50:43 +0500

    info

---
+++ js_info__1-2/2/9.js
@@ -0,0 +1,26 @@
+/*
+
+JS quirks
+
+1 how does it work 'the string' comparision?
+2 what is the unicode (unicode order)?
+3 comparision different types? (true, false)
+4 num 0 and str '0' == ?, explain
+5 difference == and === (regular equality vs strict equality)(non strict checkquirks - strict check)
+6 strict non-equality
+7 null and undefined comparision
+8 null vs 0 - ==, >, >=
+9 undefined vs 0 - ==, >, >=
+10 == === > which convert number and which not
+11 string comparision, dictionary - number
+
+
+5 > 4
+"apple" > "pineapple"
+"2" > "12"
+undefined == null
+undefined === null
+null == "\n0\n"
+null === +"\n0\n"
+
+*/
