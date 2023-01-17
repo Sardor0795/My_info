a705fa55f715a0a0c11ec075450281234d223547
Sardor0795 <english.programmer.9595@gmail.com>
2023-01-17 21:18:24 +0500

    function declaration vs function expression

--- js_info__1-2/2/16.js
+++ js_info__1-2/2/16.js
@@ -24,4 +24,40 @@ The answer is simple: a Function Expression is created here as function(…) {�
 
 The semicolon would be there for a simpler assignment, such as let sayHi = 5;, and it’s also there for a function assignment.
 
+function expression
+anonymous function
+
+A function is a value representing an “action”
+Regular values like strings or numbers represent the data.
+
+A function can be perceived as an action.
+
+We can pass it between variables and run when we want.
+
+A Function Expression is created when the execution reaches it and is usable only from that moment.
+
+A Function Declaration can be called earlier than it is defined.
+
+That’s due to internal algorithms. When JavaScript prepares to run the script, it first looks for global Function Declarations in it and creates the functions. We can think of it as an “initialization stage”.
+
+Another special feature of Function Declarations is their block scope.
+
+In strict mode, when a Function Declaration is within a code block, it’s visible everywhere inside that block. But not outside of it.
+
+let i;
+
+function n() {
+  i = function name(params) {
+    console.log(55);
+  };
+}
+
+n();
+
+i();
+
+
+block scope
+func declaration in block scope
+
 */
