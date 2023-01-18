6828d23cfe972df6413d396f9436f82009849794
Sardor0795 <english.programmer.9595@gmail.com>
2023-01-18 17:16:01 +0500

    ninja code

---
+++ js_info__1-2/3/4.js
@@ -0,0 +1,26 @@
+/*
+
+Ninja code
+
+Learning without thought is labor lost; thought without learning is perilous.
+
+Brevity is the soul of wit
+Make the code as short as possible. Show how smart you are.
+
+
+Use abbreviations
+If the team rules forbid the use of one-letter and vague names – shorten them, make abbreviations.
+
+Like this:
+
+list → lst.
+userAgent → ua.
+browser → brsr.
+…etc
+
+
+While choosing a name try to use the most abstract word. Like obj, data, value, item, elem and so on.
+The ideal name for a variable is data. Use it everywhere you can. Indeed, every variable holds data, right?
+
+…But what to do if data is already taken? Try value, it’s also universal. After all, a variable eventually gets a value.
+*/
