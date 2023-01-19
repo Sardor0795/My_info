65b2dafa73c88cc45e73b58b60c766e418032336
Sardor0795 <english.programmer.9595@gmail.com>
2023-01-19 08:10:12 +0500

    info

---
+++ js_info__1-2/3/6.js
@@ -0,0 +1,12 @@
+/*
+
+As programmers, we’d like to use most recent features. The more good stuff – the better!
+
+On the other hand, how to make our modern code work on older engines that don’t understand recent features yet?
+
+There are two tools for that:
+
+Transpilers.
+Polyfills.
+
+*/
