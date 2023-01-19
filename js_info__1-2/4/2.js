afdc2bb266c7e843c143ed92cad0b2eb55be33d4
Sardor0795 <english.programmer.9595@gmail.com>
2023-01-19 21:40:13 +0500

    obj clone

---
+++ js_info__1-2/4/2.js
@@ -0,0 +1,45 @@
+/*
+
+For comparisons like obj1 > obj2 or for a comparison against a primitive obj == 5, objects are converted to primitives. We’ll study how object conversions work very soon, but to tell the truth, such comparisons are needed very rarely – usually they appear as a result of a programming mistake.
+
+const can be modified
+
+It might seem that the line (*) would cause an error, but it does not. The value of user is constant, it must always reference the same object, but properties of that object are free to change.
+
+In other words, the const user gives an error only if we try to set user=... as a whole.
+
+
+cloning primitive level , nested clonng
+
+
+obj cloning 4 ways
+4th spread operatio
+
+There are also other methods of cloning an object, e.g. using the spread syntax clone = {...user}, covered later in the tutorial.
+
+
+Nested cloning
+Until now we assumed that all properties of user are primitive. But properties can be references to other objects.
+
+
+implements
+
+
+The structuredClone method can clone most data types, such as objects, arrays, primitive values.
+
+It also supports circular references, when an object property references the object itself (directly or via a chain or references).
+
+let user = {};
+// let's create a circular reference:
+// user.me references the user itself
+user.me = user;
+
+let clone = structuredClone(user);
+alert(clone.me === clone); // true
+
+
+Although, there are cases when structuredClone fails.
+
+For instance, when an object has a function property:
+
+*/
