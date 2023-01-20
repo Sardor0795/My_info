c8a54555f70003243ac6df1dbf1055c73d3de365
Sardor0795 <english.programmer.9595@gmail.com>
2023-01-20 18:01:14 +0500

    info

---
+++ js_info__1-2/4/8.js
@@ -0,0 +1,12 @@
+/*
+
+Object to primitive conversion
+
+There’s no conversion to boolean. All objects are true in a boolean context, as simple as that. There exist only numeric and string conversions.
+
+let obj = {};
+
+console.log(Boolean(obj));
+
+
+*/
