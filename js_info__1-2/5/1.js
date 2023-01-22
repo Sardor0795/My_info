05b1e4b8f6af646c1a06d1c26f3b04ee93b49490
Sardor0795 <english.programmer.9595@gmail.com>
2023-01-22 17:27:36 +0500

    info

---
+++ js_info__1-2/5/1.js
@@ -0,0 +1,6 @@
+/*
+
+Methods of primitives
+JavaScript allows us to work with primitives (strings, numbers, etc.) as if they were objects. They also provide methods to call as such. We will study those soon, but first we’ll see how it works because, of course, primitives are not objects (and here we will make it even clearer).
+
+*/
